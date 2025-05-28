5208a3e4f131b7a8ffac63e6917ee5262041449e
Enochteo <enochowoade@gmail.com>
2025-05-28 12:21:51 -0500

    add api and request

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+from flask import Flask, render_template, request
 import requests
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    return render_template("weather.html")
+API_KEY = os.getenv("API_KEY")
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/', methods=['GET', 'POST'])
+def weather():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city']
+        params = {
+            'q': city,
+            'appid': API_KEY,
+            'units': 'imperial'
+        }
+        response = requests.get(BASE_URL, params=params)
+        if response.status_code == 200:
+            r = response.json()
+            weather_data = {
+                'city': city,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon']
+            }
+        else:
+            weather_data = {'error': 'City not found'}
+    return render_template('weather.html', weather=weather_data)
